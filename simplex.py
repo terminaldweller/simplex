653199d8094573f7ee1c6a0689cab9fd15f1d99f
terminaldweller <thabogre@gmail.com>
2022-06-10 14:48:13 +0430

    generating A and b

--- simplex.py
+++ simplex.py
@@ -5,9 +5,9 @@ import argparse
 import ast
 import dataclasses
 import logging
-
-# import numba
-# import numpy
+import numba as nb  # type:ignore
+import numpy as np
+import sys
 import typing
 
 
@@ -22,6 +22,14 @@ class Argparser:  # pylint: disable=too-few-public-methods
             type=str,
             help="the file containing the equations",
             default=False,
+        )
+        parser.add_argument(
+            "--slack",
+            "-s",
+            type=str,
+            help="slack variable base name, names are creted"
+            "by adding a number to the string",
+            default="xx",
         )
         parser.add_argument(
             "--min",
@@ -50,25 +58,17 @@ class Argparser:  # pylint: disable=too-few-public-methods
 
 @dataclasses.dataclass
 class Equation:
-    vars_mults: typing.TypedDict("VarMult", {"var": str, "mult": float})
+    vars_mults: typing.Dict
     op: str
     rhs: float
 
-    def autofill_one_mults(self):
-        for k, v in self.vars_mults:
-            if v is None:
-                v = 1
 
-    def dump_var_mults(self):
-        for k, v in self.vars_mults:
-            print(k, v)
-
-
-def walk_expr(
+@typing.no_type_check
+def expr_visitor(
     node: typing.Any, equ: Equation, debug: bool
-) -> (typing.Any, Equation):
+) -> typing.Tuple[typing.Any, Equation]:
     """walks over a python expression and extracts the
-    operand,identifiers and literals"""
+    operands, identifiers and literals"""
     if node is None:
         return None, equ
 
@@ -99,8 +99,8 @@ def walk_expr(
             elif isinstance(ast_node.op, ast.Add):
                 if debug:
                     print("+")
-            walk_expr(ast_node.left, equ, debug)
-            walk_expr(ast_node.right, equ, debug)
+            expr_visitor(ast_node.left, equ, debug)
+            expr_visitor(ast_node.right, equ, debug)
         case ast.Compare:
             if isinstance(ast_node.ops[0], ast.LtE):
                 if debug:
@@ -133,17 +133,19 @@ def walk_expr(
                         type(ast_node.comparators[0])
                     )
                 )
-            walk_expr(ast_node.left, equ, debug)
+                sys.exit(1)
+            expr_visitor(ast_node.left, equ, debug)
         case ast.Name:
             if debug:
                 print(ast_node.id)
             if ast_node.id not in equ.vars_mults:
                 equ.vars_mults[ast_node.id] = 1
         case ast.Constant:
+            # We don't have anything to do here
             if debug:
                 print(ast_node.value)
         case ast.Expr:
-            walk_expr(ast_node.value, equ, debug)
+            expr_visitor(ast_node.value, equ, debug)
         case ast.UnaryOp:
             if isinstance(ast_node.op, ast.USub):
                 if debug:
@@ -159,42 +161,134 @@ def walk_expr(
                         type(ast_node.op)
                     )
                 )
+                sys.exit(1)
         case _:
             logging.fatal(
                 "encountered unexpected node,{} , in ast".format(
                     type(ast_node)
                 )
             )
+            sys.exit(1)
 
     return None, equ
 
 
-def parse_equations(equ_file: str, debug: bool):
+def parse_equations(equ_file: str, debug: bool) -> typing.List[Equation]:
     """parses the equations as pyithon expressions"""
     equs = []
     with open(equ_file, encoding="utf-8") as equ_expr:
         equs_unparsed = equ_expr.readlines()
-        for equ in equs_unparsed:
-            equ_parsed = ast.parse(equ)
+        for equ_unparsed in equs_unparsed:
+            equ_parsed = ast.parse(equ_unparsed)
             if debug:
                 print(type(equ_parsed.body[0]))
                 print(ast.dump(equ_parsed.body[0], indent=4))
-            equ = Equation(dict(), "", "")
-            _, res = walk_expr(equ_parsed.body[0], equ, debug)
+            equ = Equation(dict(), "", 0.0)
+            _, res = expr_visitor(equ_parsed.body[0], equ, debug)
             equs.append(res)
 
     if debug:
-        for equ in equs:
-            print("vars_mults:", equ.vars_mults)
-            print("op: ", equ.op)
-            print("rhs: ", equ.rhs)
-            print()
+        [print(equ) for equ in equs]
+
+    return equs
+
+
+def buildA(
+    equs: typing.List[Equation], verbose: bool, slack: str
+) -> typing.Tuple[np.ndarray, np.ndarray]:
+    """build the A matrix, adding the slack variable along the way"""
+    var_list = dict()
+    slack_counter: int = 1
+
+    equ_b = []
+    binds = []
+    for i, equ in enumerate(equs):
+        if len(equ.vars_mults) == 1:
+            binds.append(equ)
+            continue
+
+        if equ.op == "<=" or equ.op == "<":
+            if len(equ.vars_mults) > 1:
+                equ.vars_mults[slack + repr(slack_counter)] = 1
+                slack_counter += 1
+            if equ.op == "<=":
+                binds.append(Equation({slack + repr(i): 1}, ">=", 0.0))
+            else:
+                binds.append(Equation({slack + repr(i): 1}, ">", 0.0))
+        elif equ.op == ">=" or equ.op == ">":
+            if len(equ.vars_mults) > 1:
+                equ.vars_mults[slack + repr(slack_counter)] = -1
+                slack_counter += 1
+            if equ.op == ">=":
+                binds.append(Equation({slack + repr(i): 1}, ">=", 0.0))
+            else:
+                binds.append(Equation({slack + repr(i): 1}, ">", 0.0))
+        elif equ.op == "=":
+            pass
+        elif equ.op == "":
+            equ_b.append(equ)
+        else:
+            logging.fatal("found unexpected operand, {}".format(equ.op))
+            sys.exit(1)
+
+        for key, val in equ.vars_mults.items():
+            var_list[key] = True
+
+    equs.remove(equ_b[0])
+    for bind in binds:
+        if bind in equs:
+            equs.remove(bind)
+
+    if len(equ_b) == 0:
+        logging.fatal("did not provide a cost equation.")
+        sys.exit(1)
+
+    if verbose:
+        print("cost:")
+        print(equ_b[0])
+        print("equations:")
+        [print(equ) for equ in equs]
+        print("binds:")
+        [print(bind) for bind in binds]
+        print(var_list)
+
+    m: int = len(equs)
+    n: int = len(var_list)
+
+    if verbose:
+        print("m: {}, n: {}".format(m, n))
+
+    var_names: typing.List = []
+    for key, _ in var_list.items():
+        var_names.append(key)
+    var_names.sort()
+
+    # [print(var) for var in var_names]
+    A: np.ndarray = np.ndarray((m, n))
+    b: np.ndarray = np.ndarray((m, 1))
+    for i in range(0, m):
+        for j in range(0, n):
+            if var_names[j] in equs[i].vars_mults:
+                A[i][j] = equs[i].vars_mults[var_names[j]]
+            else:
+                A[i][j] = 0
+            pass
+
+    for i in range(0, m):
+        b[i] = equs[i].rhs
+
+    if verbose:
+        print(A)
+        print(b)
+
+    return A, b
 
 
 def main() -> None:
     """the entry point for the module"""
     argparser = Argparser()
-    parse_equations(argparser.args.equs, argparser.args.debug)
+    equs = parse_equations(argparser.args.equs, argparser.args.debug)
+    A, b = buildA(equs, argparser.args.verbose, argparser.args.slack)
 
 
 if __name__ == "__main__":
